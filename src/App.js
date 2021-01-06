5e3f4ecd7bb3d4755095bca833c376ff7453d06a
7dngur7 <7dngur7@naver.com>
2021-01-06 22:23:36 +0900

    potato bb

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Potato from "./potato";
 
 function App() {
-  return <div>hello!</div>;
+  return <div>
+    <h1>hello</h1>
+    <Potato />
+    </div>;
 }
 
 export default App;
