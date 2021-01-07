9d8a6ebd728a9344e3ef52d812fd03604252d1b2
7dngur7 <7dngur7@naver.com>
2021-01-07 15:27:46 +0900

    class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
-function Food({fav}){
-  return <h1>I like {fav}</h1>;
-}
-
-function App() {
-  return(
-    <div>
-    <h1>hello</h1>
-    <Food fav="kichi" />
-    </div>);
+class App extends React.Component {
+  render(){
+    return <h1>I am class</h1>
+  }
 }
 
 export default App;
