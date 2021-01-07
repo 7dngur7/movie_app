04f96f74e0206284e4686925d1f945630c1a193d
7dngur7 <7dngur7@naver.com>
2021-01-07 18:00:36 +0900

    component mount, update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react';
 
 class App extends React.Component {
+  state = {
+    count : 0
+  };
+  add = () => {
+    this.setState(current =>({count:current.count+1}))
+  };
+  minus = () =>{
+    this.setState(current =>({count:current.count-1}))
+  };
+  componentDidMount(){
+    console.log("didmount");
+  }
+  componentDidUpdate(){
+    console.log("update now");
+  }
+  componentWillUnmount(){
+    console.log("unmount");
+  }
   render(){
-    return <h1>I am class</h1>
+    console.log("render");
+    return (
+      <div>
+        <h1>the number is : {this.state.count}</h1>
+        <button onClick = {this.add}>add</button>
+        <button onClick = {this.minus}>minus</button>
+      </div>
+    )
+    
   }
 }
 
